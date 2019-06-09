15c29a4de7b264cae1167bee372a56102d75dca2
Free Lance Projects <freelancepjts@gmail.com>
2019-06-09 13:16:17 +0530

    strings practice

--- strings/src/test/java/com/strings/StringsPractice.groovy
+++ strings/src/test/java/com/strings/StringsPractice.groovy
@@ -1,4 +1,15 @@
 package com.strings
 
+import org.junit.Test
+
 class StringsPractice {
+
+    @Test
+    public void declareString(){
+        def name1 = "jack"
+        println "name1=$name1"
+
+        String name2 = "john"
+        println "name2=$name2"
+    }
 }
